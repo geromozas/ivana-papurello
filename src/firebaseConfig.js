0b36fb51126311a5b961cd55732b57b0265dd2dd
Geronimo Mozas <mozasgeronimo@gmail.com>
2025-03-12 16:18:59 -0300

    base de proyecto ivana con conexion a firebase

---
+++ src/firebaseConfig.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDLYnN9AZksfUXKhFC7PCap4u8vJUpeQmE",
+  authDomain: "ivana-papurello.firebaseapp.com",
+  projectId: "ivana-papurello",
+  storageBucket: "ivana-papurello.firebasestorage.app",
+  messagingSenderId: "199396629923",
+  appId: "1:199396629923:web:ad11ed7aad78a3e01cce16",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
